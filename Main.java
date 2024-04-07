8467e1dbd15b40a9525f720a1b7861bc6d65eea9
Impala67SS <162022376+Impala67SS@users.noreply.github.com>
2024-04-07 11:15:41 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+                Point point = new Point(7, 9);
+
+System.out.println("Lokalizacja:");
+
+System.out.println("x = " + point.getX());
+
+System.out.println("y = " + point.getY());
+
+    }
+}
